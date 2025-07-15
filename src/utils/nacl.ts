73db91d80239a88309009a5bbf73a6dca33e5e2d
MoMannn <tadej.vengust@consensys.net>
2025-07-15 16:27:14 +0200

    Update major version of some dependencies.

--- src/utils/nacl.ts
+++ src/utils/nacl.ts
@@ -1,6 +1,6 @@
-import { randomBytes } from '@noble/ciphers/crypto';
-import { hsalsa, secretbox } from '@noble/ciphers/salsa';
-import { u32, u8 } from '@noble/ciphers/utils';
+import { randomBytes } from '@noble/ciphers/webcrypto.js';
+import { hsalsa, secretbox } from '@noble/ciphers/salsa.js';
+import { u32, u8 } from '@noble/ciphers/utils.js';
 import { x25519 } from '@noble/curves/ed25519';
 
 const PUBLIC_KEY_BYTES = 32;
